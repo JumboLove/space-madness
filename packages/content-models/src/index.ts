559d6fc129e6fd8d8370bb35b899ab0b4ea3b434
David Witt <midknight55@gmail.com>
2023-06-07 11:56:05 -0700

    Added Shadcn UI

--- packages/content-models/src/index.ts
+++ packages/content-models/src/index.ts
@@ -3,3 +3,4 @@ export * from "./post";
 export * from "./blockContent/callout";
 export * from "./blockContent/image";
 export * from "./blockContent/figure";
+export * from "./blockContent/internalLink";
