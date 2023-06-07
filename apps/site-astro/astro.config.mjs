0b91fa4ae982a8f93ab160f12597ea972126c34d
David Witt <midknight55@gmail.com>
2023-06-07 11:40:36 -0700

    Added tailwind

--- apps/site-astro/astro.config.mjs
+++ apps/site-astro/astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import sanity from "astro-sanity";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: '5k9qefjb',
-      dataset: 'production',
-      apiVersion: '2021-03-25',
-      useCdn: true,
-    })
-  ]
+  integrations: [sanity({
+    projectId: '5k9qefjb',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true
+  }), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })]
 });
