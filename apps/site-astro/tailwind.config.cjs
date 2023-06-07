4d5a25f6d4edde5ec460f3fc7cf37d1c6dab6fa9
David Witt <midknight55@gmail.com>
2023-06-07 11:59:10 -0700

    Fixed some issues in minor refactor

--- apps/site-astro/tailwind.config.cjs
+++ apps/site-astro/tailwind.config.cjs
@@ -52,7 +52,7 @@ module.exports = {
 				sm: "calc(var(--radius) - 4px)",
 			},
 			fontFamily: {
-				sans: ["var(--font-sans)", ...fontFamily.sans],
+				sans: ["var(--font-sans)"],
 			},
 			keyframes: {
 				"accordion-down": {
