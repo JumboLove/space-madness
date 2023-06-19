601784eeb656c35b7d329f4cd13f4927b983bec8
David Witt <midknight55@gmail.com>
2023-06-19 11:28:02 -0700

    Very basic version of the Site Search Dialog is working

--- apps/site-astro/src/components/SiteSearchDialog.tsx
+++ apps/site-astro/src/components/SiteSearchDialog.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { useState, useEffect } from "react";
 import {
   Calculator,
   Calendar,
@@ -9,6 +9,7 @@ import {
 } from "lucide-react";
 
 import {
+  Command,
   CommandDialog,
   CommandEmpty,
   CommandGroup,
@@ -19,51 +20,96 @@ import {
   CommandShortcut,
 } from "@/components/ui/command";
 
-export function SiteSearchDialog() {
-  const [open, setOpen] = React.useState(false);
+// pagefind will be added to the window object from SiteSearch.astro
+declare global {
+  interface Window {
+    pagefind: any;
+  }
+}
 
-  React.useEffect(() => {
+type PagefindResult = {
+  id: string;
+  data: () => {};
+  excerpt_range: number[];
+  score: number;
+  words: number[];
+};
+
+type PagefindData = {
+  id: string;
+  url: string;
+  content: string;
+  word_count: number;
+  filters: object;
+  meta: {
+    title: string;
+  };
+  raw_content: string;
+  excerpt: string;
+  raw_url: string;
+};
+
+export function SiteSearchDialog() {
+  const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState<PagefindData[]>([]);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  async function runSearch(searchVal) {
+    // setSearch(searcVal);
+    console.log(searchVal);
+
+    try {
+      const pagefindResults = await window.pagefind.search(searchVal);
+      if (pagefindResults.results) {
+        const dataArray: PagefindData[] = await Promise.all(
+          pagefindResults.results
+            .slice(0, 5)
+            .map(async (result: PagefindResult) => {
+              const data = await result.data();
+              return { id: result.id, ...data };
+            })
+        );
+        setResults(dataArray);
+        console.log(results);
+      } else {
+        setErrorMsg("Search results could not be loaded");
+      }
+    } catch (error) {
+      setErrorMsg("An error occurred while fetching search results");
+    }
+  }
+
+  useEffect(() => {
     document.addEventListener("openSearch", () => setOpen((open) => !open));
   }, []);
 
   return (
     <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder="Search anything..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>
-            <Calendar className="mr-2 h-4 w-4" />
-            <span>Calendar</span>
-          </CommandItem>
-          <CommandItem>
-            <Smile className="mr-2 h-4 w-4" />
-            <span>Search Emoji</span>
-          </CommandItem>
-          <CommandItem>
-            <Calculator className="mr-2 h-4 w-4" />
-            <span>Calculator</span>
-          </CommandItem>
-        </CommandGroup>
-        <CommandSeparator />
-        <CommandGroup heading="Settings">
-          <CommandItem>
-            <User className="mr-2 h-4 w-4" />
-            <span>Profile</span>
-            <CommandShortcut>⌘P</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <CreditCard className="mr-2 h-4 w-4" />
-            <span>Billing</span>
-            <CommandShortcut>⌘B</CommandShortcut>
-          </CommandItem>
-          <CommandItem>
-            <Settings className="mr-2 h-4 w-4" />
-            <span>Settings</span>
-            <CommandShortcut>⌘S</CommandShortcut>
-          </CommandItem>
-        </CommandGroup>
-      </CommandList>
+      <Command shouldFilter={false}>
+        <CommandInput
+          placeholder="Search anything..."
+          onValueChange={runSearch}
+        />
+
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandItem>{results.length}</CommandItem>
+
+          <CommandSeparator />
+          <CommandGroup heading="Results">
+            {results.length > 0 &&
+              results.map((result) => {
+                return (
+                  <CommandItem key={result.id}>
+                    <div>{result.meta.title}</div>
+                    <div dangerouslySetInnerHTML={{ __html: result.excerpt }} />
+                  </CommandItem>
+                );
+              })}
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </CommandDialog>
   );
 }
