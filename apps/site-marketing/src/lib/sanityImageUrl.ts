3715805fe8f909af123c7a94279e4cc24a7298aa
David Witt <midknight55@gmail.com>
2023-06-27 09:45:17 -0700

    Moved site-marketing to its own repo

--- apps/site-marketing/src/lib/sanityImageUrl.ts
+++
@@ -1,7 +0,0 @@
-import { createImageBuilder, useSanityClient } from "astro-sanity";
-
-export const imageBuilder = createImageBuilder(useSanityClient());
-
-export function urlForImage(source: string) {
-  return imageBuilder.image(source);
-}
