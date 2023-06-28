11b82b9cc20b60080cd27e48fa5b6142724103a2
David Witt <midknight55@gmail.com>
2023-06-28 11:43:45 -0700

    Formatted all files in site-astro

--- apps/site-astro/src/components/svg/PopoverBubble.tsx
+++ apps/site-astro/src/components/svg/PopoverBubble.tsx
@@ -8,7 +8,7 @@ export default function PopoverBubble(props: SVGAttributes<SVGSVGElement>) {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="h-6 w-6"
       {...props}
     >
       <path
